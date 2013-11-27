7e4b666c75732fd5a86f685c4fe4355de9380ea2
Gary Gordon <gfgordon@gmail.com>
2013-11-27 12:26:27 -0500

    use proc for scope

--- lib/command_status_apparatus/command_status.rb
+++ lib/command_status_apparatus/command_status.rb
@@ -2,7 +2,7 @@ class CommandStatus < ActiveRecord::Base
 
   validates :key, :presence => true
 
-  scope :ordered, order("updated_at DESC")
+  scope :ordered, -> { order("updated_at DESC") }
 
   def self.last_status(key)
     where(key: key).limit(1).ordered
