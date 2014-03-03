5f5eb4073135087a95c0578a5ed29f3f18b9ac9d
Gary Gordon <gfgordon@gmail.com>
2014-03-03 09:03:04 -0500

    handle uninitialized case

--- lib/command_status_apparatus/command_status_interface.rb
+++ lib/command_status_apparatus/command_status_interface.rb
@@ -6,18 +6,18 @@ module CommandStatusInterface
   end
 
   def call_with_command_status_logging(count)
-    @command_status_instance.total_count = count
-    @command_status_progress_bar.total = count
+    @command_status_instance.total_count = count if @command_status_instance
+    @command_status_progress_bar.total = count if @command_status_instance
     result = yield
-    @command_status_instance.success_count = @command_status_success_count
+    @command_status_instance.success_count = @command_status_success_count if @command_status_instance
     result
   end
 
   def increment_command_status_progress
     result = yield
-    @command_status_progress_bar.try(:increment)
-    @command_status_success_count ||= 0
-    @command_status_success_count += 1 if result.present?
+    @command_status_progress_bar.try(:increment) if @command_status_instance
+    @command_status_success_count ||= 0 if @command_status_instance
+    @command_status_success_count += 1 if result.present? if @command_status_instance
     result
   end
 
